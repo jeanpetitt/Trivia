fc471614b58150953dd407b491264d2b1a1d68c9
jeanpetitt <82118752+jeanpetitt@users.noreply.github.com>
2022-08-19 14:34:32 +0100

    suppression d'une question

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -8,47 +8,92 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+# fonction qui permet de regrouper 10 questions par page
+def paginate_question(request, selection):
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE
+    questions = [question.format() for question in selection]
+    current_questions = questions[start:end]
+    return current_questions
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     setup_db(app)
+    CORS(app)
+    #headers cors
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTION"
+        )
+        return response
+        
+    # route qui affiche la liste de toutes les categories
+    @app.route('/categories', methods=['GET'])
+    def get_category():
+        categories = Category.query.all()
+        # obtenir une liste de categorie
+        list_cat = [categorie.format() for categorie in categories]
+        return jsonify({
+            'categories': list_cat,
+        })
+    
+    # chemin qui recupere toutes 10 les questions, par page, 
+    # le nombre total des question, la liste des categories courante,
+    # la liste de toutes les categories
+    @app.route('/questions')
+    def retrieve_questions():
+        # questions
+        select_question = Question.query.order_by(Question.id).all()
+        current_questions = paginate_question(request, select_question)
+        questions = Question.query.all()
+        # categories
+        select_category = Category.query.order_by(Category.id).all()
+        current_categories = paginate_question(request, select_category)
+        categories = Category.query.all()
+        
+        # obtenir une liste de categorie
+        list_cat = [categorie.format() for categorie in categories]
+        
+        if len(current_questions) == 0:
+            abort(404)
+            
+        return jsonify({
+            'success': True,
+            'questions': current_questions,
+            'total_questions': len(questions),
+            'current_category': current_categories,
+            'categories': list_cat,
+        })
 
-    """
-    @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-    """
+    # route permettant dupprimer une question
+    @app.route("/questions/<int:question_id>", methods=['DELETE'])
+    def delete_book(question_id):
+        try:
+            question = Question.query.filter(Question.id == question_id).one_or_none()
+            if question is None:
+                abort(404)
 
-    """
-    @TODO: Use the after_request decorator to set Access-Control-Allow
-    """
+            question.delete()
+            select = Question.query.order_by(Question.id).all()
+            current_questions = paginate_question(request, select)
+            return jsonify(
+                {
+                    "success": True,
+                    "deleted": question_id,
+                    "question": current_questions,
+                    "total_questions": len(Question.query.all()),
+                }
+            )
 
-    """
-    @TODO:
-    Create an endpoint to handle GET requests
-    for all available categories.
-    """
-
-
-    """
-    @TODO:
-    Create an endpoint to handle GET requests for questions,
-    including pagination (every 10 questions).
-    This endpoint should return a list of questions,
-    number of total questions, current category, categories.
-
-    TEST: At this point, when you start the application
-    you should see questions and categories generated,
-    ten questions per page and pagination at the bottom of the screen for three pages.
-    Clicking on the page numbers should update the questions.
-    """
-
-    """
-    @TODO:
-    Create an endpoint to DELETE question using a question ID.
-
-    TEST: When you click the trash icon next to a question, the question will be removed.
-    This removal will persist in the database and when you refresh the page.
-    """
-
+        except:
+            abort(422)
+    
     """
     @TODO:
     Create an endpoint to POST a new question,
